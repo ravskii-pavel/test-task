d0e3474f7539b8a2bde9648d663db9ea7c4a6cff
ravskii-pavel <ravskii.pavel@gmail.com>
2017-08-11 00:40:28 +0300

    add Test (not done), Logger, Localizations

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Properties;
@@ -8,23 +9,35 @@ import java.util.TimeZone;
 
 public class HelloWorld {
 
+    private static final Logger LOGGER = Logger.getLogger(HelloWorld.class);
     private FileInputStream fis;
     private Properties property = new Properties();
 
-
-    public void sayHello() {
-        //String timesOfDay;
-        //TimeZone timeZone = Calendar.getInstance().getTimeZone();
+    void sayHello() {
+        String messageProp;
+        TimeZone timeZone = Calendar.getInstance().getTimeZone();
+        String timeZoneName =  timeZone.getID();
+        System.out.println(timeZone.getID());
         Calendar calendar = Calendar.getInstance();
-        String propFileName = "message.resource.properties";
-        String now = calendar.getTime().toString();
+//        String propFileName = "message.resource.properties";
+//        String now = calendar.getTime().toString();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
 
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-
+        /*InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);*/
+        if (timeZoneName.equals("Europe/Kiev")) messageProp = "message.ru.properties";
+        else messageProp = "message.en.properties";
         try {
-            fis = new FileInputStream("src/main/resources/message.resource1.properties");
-            property.load(fis);
+            fis = new FileInputStream("src/main/resources/localizations/"+messageProp);
+//            BufferedReader is = new BufferedReader(new InputStreamReader(fis, "Cp1251"));
+//            BufferedReader bufReader = new BufferedReader(new InputStreamReader(new FileInputStream(String.valueOf(fis)), Charset.forName("UTF-8")));
+
+
+            //FileInputStream fis1 = new FileInputStream("priwet.txt");
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+            Reader in = new BufferedReader(isr);
+
+            property.load(in);
+            in.close();
             if (hour >= 9 && hour < 19) {
                 System.out.println(property.getProperty("prop.day"));
             }
@@ -40,6 +53,7 @@ public class HelloWorld {
 
         } catch (IOException e) {
             System.err.println("ОШИБКА: Файл свойств отсуствует!");
+            LOGGER.error("ОШИБКА: Файл свойств отсуствует!");
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args){
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.sayHello();
 
